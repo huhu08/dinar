99f51f08870a44b70327018728e833ce7cd4d4bd
Huda <hudaelzubair@hotmail.com>
2026-05-19 21:09:57 +0300

    dashboard iports

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//import { BlockchainService } from '../../services/blockchain.service';
+//import { Block } from '../../models/blockchain.models';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  imports: [CommonModule],
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent {
